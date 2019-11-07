fc80a341067b4a1c475e6e50ea661138aee8e441
akeonstar <chriswortman@gmail.com>
2019-11-06 18:40:51 -0800

    updating bot, adding quotes

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 import asyncio
 import os
+import json
+import random
 
 client = discord.Client()
 
@@ -13,6 +15,13 @@ except KeyError:
 print(os.environ["discord_bot_token"])
 secret_token = (os.environ["discord_bot_token"])
 
+
+def get_random_quote():
+   with open('quotes.json','r') as quotes:
+      bobbyb_quotes = json.load(quotes)
+   return random.choice(bobbyb_quotes)
+
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -24,9 +33,15 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!ping'):
-       await client.send_message(message.channel, 'Pong!')
-    if message.content.startswith('!ding'):
-    	await client.send_message(message.channel, 'Dong, motha fucka!')
+   channel = message.channel
+   if message.content.startswith('!ping'):
+      await channel.send( 'Pong!')
+   if message.content.startswith('!ding'):
+      await channel.send( 'Dong, motha fucka!')
+   ### The magic
+   if message.content.startswith('bobbyb'):
+      msg = get_random_quote().format(message)
+#      msg = "THANK THE GODS FOR BESSIE AND HER TITS"
+      await channel.send(msg)
 
 client.run(secret_token)
